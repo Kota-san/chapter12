ce189673556643b95cf76f8b186cfb7bc44f90bc
241019 <241019@std.hi-joho.ac.jp>
2025-07-02 09:25:16 +0900

    迫真コミット君BB

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,9 @@
-//TIP コードを<b>実行</b>するには、<shortcut actionId="Run"/> を押すか
-// ガターの <icon src="AllIcons.Actions.Execute"/> アイコンをクリックします。
 public class Main {
     public static void main(String[] args) {
-        Character[] party = new Character[3];
-        party[0] = new Hero();
-        party[1] = new Wizard();
-        party[2] = new Thief();
-
-        for (Character member : party) {
-            member.hp += 50; // 全員のHPを回復
-        }
+        Hero h = new Hero();
+        h.name = "勇者";
+        h.attack(new Matango());
+        h.attack(new Goblin());
+        h.attack(new Slime());
     }
 }
