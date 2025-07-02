0e6a36b3f3d014243a609c977cea7b8795e30e6d
241019 <241019@std.hi-joho.ac.jp>
2025-07-02 09:26:40 +0900

    迫真コミット君BB

--- src/Wizard.java
+++ src/Wizard.java
@@ -13,4 +13,10 @@ public class Wizard extends Character{
         m.hp -= 20;
         this.mp -= 5;
     }
+
+    public void attack(Monster m) {
+        System.out.println(this.name + "の攻撃！");
+        System.out.println("敵に3ポイントのダメージ");
+        m.hp -= 3;
+    }
 }
